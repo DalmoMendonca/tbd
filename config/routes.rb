213918119d0a11d71306cee2a7c1406085a24097
Dalmo <dalmomendonca@gmail.com>
2016-05-23 13:41:27 -0400

    Set up omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root :to => 'pages#home', :controller => 'pages', :action => :home
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
