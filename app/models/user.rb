7a928bed2f08343bcf6dc9e216d1fa687c5b5cc2
Dalmo <dalmomendonca@gmail.com>
2016-05-23 20:26:07 -0400

    Add access token

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
 	    user.provider         = auth.provider 
 	    user.uid              = auth.uid
 	    user.name             = auth.info.name
-	    user.oauth_token      = "EAACEdEose0cBADeAI3pbbXgb5md6nJciItCYNZBvdREgy5HahVWAIJ8rBD5K9D8NnZADZAz1j9ua7LmTOOhdNeLJvUV4TXwNGvV1rBmlXoRK0UYRYZBMlZADvDoXxo4SbiqAxbRwa0Tf3avxPwTayNZCD9XdqW3jlvppCOEcNfRQZDZD" #auth.credentials.token
+	    user.oauth_token      = auth.credentials.token #"EAACEdEose0cBADeAI3pbbXgb5md6nJciItCYNZBvdREgy5HahVWAIJ8rBD5K9D8NnZADZAz1j9ua7LmTOOhdNeLJvUV4TXwNGvV1rBmlXoRK0UYRYZBMlZADvDoXxo4SbiqAxbRwa0Tf3avxPwTayNZCD9XdqW3jlvppCOEcNfRQZDZD" 
 	    user.oauth_expires_at = Time.at(auth.credentials.expires_at)
 	    user.save
 	  end
