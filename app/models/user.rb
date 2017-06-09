3c0cb2b858fbac974c063f47a72b50e8d69170c8
Dalmo <dalmomendonca@gmail.com>
2017-06-09 19:47:18 -0400

    add tap

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
 
 	def self.from_omniauth(auth)
-  	where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+  	where(auth.slice(:provider, :uid)).first_or_create.tap do |user|
 	    user.provider = auth.provider
 	    user.uid = auth.uid
 	    user.name = auth.info.name
-	    user.oauth_token = auth.credentials.token
+	    user.oauth_token = auth.credentials.token unless auth.credentials.token.nil?
 	    user.oauth_expires_at = Time.at(auth.credentials.expires_at) unless auth.credentials.expires_at.nil?
 	    user.save!
   	end
