7c106d9dc6549c1a9e7336a497549d8fab723d01
Dalmo <dalmomendonca@gmail.com>
2017-06-10 11:03:23 -0400

    add tap

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'],
-  	scope: "user_friends"
+  	scope: "email,user_friends"
 end
