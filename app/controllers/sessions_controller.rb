e7fa5a655f0004d387f4e3ef7c377fb787f898e7
Dalmo <dalmomendonca@gmail.com>
2017-06-10 11:43:48 -0400

    add tap

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,17 @@ class SessionsController < ApplicationController
     redirect_to root_url
   end
 
+  def menu
+    if session["fb_access_token"].present?
+      graph = Koala::Facebook::GraphAPI.new(session["fb_access_token"]) # Note that i'm using session here
+      @profile_image = graph.get_picture("me")
+      @fbprofile = graph.get_object("me")
+      @friends = graph.get_connections("me", "friends")
+      @friendsnumber = graph.get_connections("me", "friends").size
+      redirect_to root_url
+    end
+  end
+
   def destroy
     session[:user_id] = nil
     redirect_to root_url
